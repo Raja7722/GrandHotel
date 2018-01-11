65fc1f5e1241530def406c530de3f34a11662faf
MauriceHenriet <maurice.henriet@yahoo.fr>
2018-01-11 11:05:49 +0100

    test

--- GrandHotel/GrandHotel/Entites.cs
+++ GrandHotel/GrandHotel/Entites.cs
@@ -20,7 +20,7 @@ namespace GrandHotel
 
         public class Telephone
         {
-            public string Numero; // P
+            public string Numero1; // P
             public int IdClient; // F
             public string CodeType;
             public Boolean Pro;
